f4e3d68ce83bfb5fab3a30f2d0bf2270d4c73339
Angela Santo <ithisia@gmail.com>
2020-04-03 20:47:07 -0700

    Week 4 OATH

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '548532149126334',
+        clientSecret: '12aa0aa810f889b9f40dd1f48baea8be'
+    }
 }
